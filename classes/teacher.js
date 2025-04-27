6a6a887c92df22c755087011291a7805a5cc0527
SpiritGun91 <spiritgun@vivaldi.net>
2025-04-27 12:30:36 -0400

    Implement Teacher class extending Person

--- classes/teacher.js
+++ classes/teacher.js
@@ -1,12 +1,25 @@
-const Person = require('./person');
+const Person = require("./person");
 
 // Your code here
+class Teacher extends Person {
+	constructor(firstName, lastName, subject, yearsOfExperience) {
+		super(firstName, lastName);
+		this.subject = subject;
+		this.yearsOfExperience = yearsOfExperience;
+	}
+
+	static combinedYearsOfExperience(teachers) {
+		return teachers.reduce((sum, teacher) => {
+			return sum + teacher.yearsOfExperience;
+		}, 0);
+	}
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
 try {
-  module.exports = Teacher;
+	module.exports = Teacher;
 } catch {
-  module.exports = null;
+	module.exports = null;
 }

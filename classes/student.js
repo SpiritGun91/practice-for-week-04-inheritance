524f6678bdffe0ee54c0b8fa40bba53c6eeb478f
SpiritGun91 <spiritgun@vivaldi.net>
2025-04-27 12:21:17 -0400

    Implement Student class with inheritance and GPA comparison

--- classes/student.js
+++ classes/student.js
@@ -1,12 +1,26 @@
-const Person = require('./person');
+const Person = require("./person");
 
 // Your code here
+class Student extends Person {
+	constructor(firstName, lastName, major, GPA) {
+		super(firstName, lastName);
+		this.major = major;
+		this.GPA = GPA;
+	}
+
+	static compareGPA(student1, student2) {
+		if (student1.GPA === student2.GPA) return "Both students have the same GPA";
+		if (student1.GPA > student2.GPA)
+			return `${student1.firstName} ${student1.lastName} has the higher GPA.`;
+		return `${student2.firstName} ${student2.lastName} has the higher GPA.`;
+	}
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
 try {
-  module.exports = Student;
+	module.exports = Student;
 } catch {
-  module.exports = null;
+	module.exports = null;
 }
